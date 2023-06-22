56cb52f4af0a744ac8f068ef3964566b512339cb
ChandraKarthik07 <99110756+ChandraKarthik07@users.noreply.github.com>
2023-06-22 20:17:00 +0530

    7

--- search/urls.py
+++ search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import search_view,fill_models_from_csv
 
 urlpatterns = [
-    path('search/', search_view, name='search'),
-    path('fill_models/', fill_models_from_csv, name='fill'),
+    path('', search_view, name='search'),
+    path('fill_models/', fill_models_from_csv, name='fill"dont use this" '),
 
 ]
